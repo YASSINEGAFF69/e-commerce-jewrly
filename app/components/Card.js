bd303da8defd1f9b09d50b4e8a9cc9b06b932f76
Simon Bae <yassineGAFF@gmail.com>
2024-07-28 11:11:07 +0100

    bug fixing

--- app/components/Card.js
+++ app/components/Card.js
@@ -2,16 +2,18 @@ import Image from "next/image";
 import Link from 'next/link';
 
 function Card({ product }) {
+  const imageUrl = product?.images?.[0] || '/path/to/placeholder-image.jpg';
+
   return (
     <div>
       <Link href={`details/${product?.slug}`}>
         <div className="product-card">
           <Image
-            src={product?.images[0]}
+            src={imageUrl}
             width={250}
             height={250}
             className="product-image"
-            alt={product?.name}
+            alt={product?.name || 'Product Image'}
           />
           <p className="product-name">{product?.name}</p>
           <p className="product-price">{product?.price} DT</p>
--- app/components/Header.js
+++ app/components/Header.js
@@ -1,46 +1,38 @@
-"use client"
-import Image from "next/image"
+"use client";
+import Image from "next/image";
 import { FaShoppingCart } from "react-icons/fa";
-import { MdLocalShipping } from "react-icons/md";
 import { UserButton } from "@clerk/nextjs";
 import useCartStore from "../cartStore";
-import Link from 'next/link';
-import logo0 from '../../public/logo0.png';
+import Link from "next/link";
 
 function Header() {
   const totalItems = useCartStore((state) => state.totalItems);
+
   return (
     <div className="p-3 border-b-2 border-[#F5F3FF]">
-        <div className="max-w-7xl mx-auto flex justify-between">
-         
+      <div className="max-w-7xl mx-auto flex justify-between">
         <Link href="/">
-        <div className="flex items-center cursor-pointer">
-          <Image src={logo0} alt="Logo" width={200} height={50} className="rounded-md"/>
-        </div>
+          <div className="flex items-center cursor-pointer">
+            <Image src="/logo0.png" alt="Logo" width={200} height={50} className="rounded-md"/>
+          </div>
         </Link>
 
         <div className="flex items-center relative">
-            <Link href="/cart">
-             <FaShoppingCart className="text-3xl text-[#6E4F3F] cursor-pointer hover:scale-125 transition-transform duration-300" />
-            </Link>
-             {
-                  totalItems > 0 && (
-                      <div className="ml-2 bg-[#718396] rounded-full w-5 h-5 flex items-center justify-center text-white text-xs font-semibold">
-                          {totalItems}
-                      </div>
-                  )
-             }
-
-             
-
-            <div className="ml-4">
-            <UserButton  afterSignOutUrl="/"/>
+          <Link href="/cart">
+            <FaShoppingCart className="text-3xl text-[#6E4F3F] cursor-pointer hover:scale-125 transition-transform duration-300" />
+          </Link>
+          {totalItems > 0 && (
+            <div className="ml-2 bg-[#718396] rounded-full w-5 h-5 flex items-center justify-center text-white text-xs font-semibold">
+              {totalItems}
             </div>
+          )}
+          <div className="ml-4">
+            <UserButton afterSignOutUrl="/" />
+          </div>
         </div>
-
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
